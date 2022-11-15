741cbafd4f801b696f7786e46ab4a5494f1d5f45
Gabriel Dison De Lima Reis <gabriel.disonreis@gmail.com>
2022-11-15 12:14:07 -0300

    feat: fix bug in page history and finished app

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,11 @@
-import { createContext, ReactNode, useState, useReducer } from "react";
+import { differenceInSeconds } from "date-fns";
+import {
+  createContext,
+  ReactNode,
+  useState,
+  useReducer,
+  useEffect,
+} from "react";
 import {
   ActionTypes,
   addNewCycleAction,
@@ -13,7 +20,7 @@ interface CreateCycleData {
 }
 
 interface CyclesContextProps {
-  cyclesState: Cycle;
+  cycles: Cycle[];
   activeCycle: Cycle | undefined;
   activeCycleId: string | null;
   amountSecondsPassed: number;
@@ -32,16 +39,39 @@ export const CyclesContext = createContext({} as CyclesContextProps);
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  });
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    () => {
+      const storedStateAsJSON = localStorage.getItem(
+        "@ignite-timer:cycles-state-1.0.0"
+      );
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON);
+      }
+    }
+  );
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState);
+    localStorage.setItem("@ignite-timer:cycles-state-1.0.0", stateJSON);
+  }, [cyclesState]);
 
   const { activeCycleId, cycles } = cyclesState;
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
+    }
+
+    return 0;
+  });
 
   function createNewCycle(data: CreateCycleData) {
     const id = String(new Date().getTime());
@@ -72,7 +102,7 @@ export function CyclesContextProvider({
   return (
     <CyclesContext.Provider
       value={{
-        cyclesState,
+        cycles,
         activeCycle,
         activeCycleId,
         markCurrentCycleAsFinished,
